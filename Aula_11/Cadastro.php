2177a8f86f295b96dc0aeea2e3cc5a225bc0d214
Cursos <Cursos@LABFLEXXO.LOCAL>
2026-05-21 22:02:25 -0300

    teste

--- Aula_11/Cadastro.php
+++ Aula_11/Cadastro.php
@@ -20,38 +20,38 @@ if(isset($_POST["btn"])){
 
     if(empty($nome)){
       echo "<script> alert('Campo nome em branco');
-       window.location.href='PaginaLoguin1.php'; </script>";
+       window.location.href='Cadastro.php'; </script>";
        exit;
     }
     if(empty(trim($cpf))){
       echo "<script> alert('Campo cpf em branco');
-       window.location.href='PaginaLoguin1.php'; </script>";
+       window.location.href='Cadastro.php'; </script>";
        exit;
     }
 
     if(empty(trim($endereço))){
       echo "<script> alert('Campo endereço em branco');
-       window.location.href='PaginaLoguin1.php'; </script>";
+       window.location.href='Cadastro.php'; </script>";
        exit;
     }
     if(empty(trim($nivel))){
       echo "<script> alert('Campo nivel em branco');
-       window.location.href='PaginaLoguin1.php'; </script>";
+       window.location.href='Cadastro.php'; </script>";
        exit;
     }
     if(empty(trim($email))){
       echo "<script> alert('Campo email em branco');
-       window.location.href='PaginaLoguin1.php'; </script>";
+       window.location.href='Cadastro.php'; </script>";
        exit;
     }
     if(empty(trim($senha))){
       echo "<script> alert('Campo senha em branco');
-       window.location.href='PaginaLoguin1.php'; </script>";
+       window.location.href='Cadastro.php'; </script>";
        exit;
     }
      if(empty(trim($status))){
       echo "<script> alert('Campo status em branco');
-       window.location.href='PaginaLoguin1.php'; </script>";
+       window.location.href='Cadastro.php'; </script>";
        exit;
     }
     
