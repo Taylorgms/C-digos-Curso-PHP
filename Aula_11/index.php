5c519c0876b420c3061e394a077565c7103f4794
Cursos <Cursos@LABFLEXXO.LOCAL>
2026-05-21 22:00:39 -0300

    teste

---
+++ Aula_11/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Aula 11</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>   
+
+    <div class="container">
+
+    <?php require "topo.php"; ?>  
+  
+    </div>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    
+</body>
+</html>
---
+++ Aula_11/topo.php
@@ -0,0 +1,21 @@
+<div>
+
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php"> nicio </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="Cadastro.php"> Cadastro </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="sobre.php"> Sobre </a>
+        </li>
+        
+        <li class="nav-item">
+            <a class="nav-link" href="sair.php"> Sair </a>
+        </li>
+    </ul>
+
+</div>
